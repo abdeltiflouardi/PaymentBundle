dcf022dcc244e861e281dfa0d41ae0165158ce9f
LOUARDI Abdeltif <louardi.abdeltif@yahoo.fr>
2011-12-14 13:45:43 +0000

    Integrate bundle's files

---
+++ OSPaymentBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OS\PaymentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OSPaymentBundle extends Bundle
+{
+}
---
+++ PaymentFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OS\PaymentBundle;
+
+class PaymentFactory
+{
+    
+}
